e99292cfbc196bad96e372c694b0073367d43f1a
Ubuntu <ubuntu@ip-172-31-90-95.ec2.internal>
2020-01-23 15:08:55 +0000

    added landing page

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"]= 'mongodb+srv://ayaanh221:Ayaan123@myfirstcluster-lj4yz.mongodb.net/diary-max?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", task=mongo.db.tasks.find())
 
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
